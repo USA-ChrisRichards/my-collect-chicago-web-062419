1c38eb7d011f3d854158bdae9a411ae482160383
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-20 16:16:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   final_collection = []
   n = 0
   while n < collection.size
-    yield(collection[n]) << final_collection
+    collection[n] << final_collection
     n = n + 1
   end
    final_collection
