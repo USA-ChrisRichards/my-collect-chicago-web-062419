fb978ca69a8b2e6d0ab8b9e68c93669d6d30a0f2
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-20 16:25:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,13 +9,4 @@ def my_collect(collection)
    final_collection
 end
 
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
-
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
-end
  
