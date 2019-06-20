b1c68a2c094c4f53b4b1bf54d541f34107a1c04e
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-20 16:20:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,6 +8,7 @@ def my_collect(collection)
    final_collection
 end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 my_collect(array) do |name|
-  name.split(" ").first
+  print name.split(" ").first
 end
